cd16951b0ac8480419f00b9c48bf6071ed8673c0
Calleu Fuzi <calleufuzi@gmail.com>
2020-05-26 23:52:32 -0300

    feat: created method to insert message in db

--- app/sender.py
+++ app/sender.py
@@ -1,10 +1,28 @@
+import psycopg2
 from bottle import request, run, route
+
+DNS = 'dbname=email_sender user=postgres host=db'
+SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s, %s)'
+
+
+def register_message(assunto, mensagem):
+    conn = psycopg2.connect(DNS)
+    cur = conn.cursor()
+    cur.execute(SQL, (assunto, mensagem))
+    conn.commit()
+    cur.close()
+    conn.close()
+
+    print('Mensagem Registrada !')
 
 
 @route('/', method='POST')
 def send():
     assunto = request.forms.get('assunto')
     mensagem = request.forms.get('mensagem')
+
+    register_message(assunto, mensagem)
+
     return 'Mensagem enfileirada ! Assunto: {} Mensagem: {}'.format(
         assunto, mensagem
     )
